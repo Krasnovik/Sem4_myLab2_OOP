dd8a6c5c4fabc794ff01261b627db7f7ccd25e6d
Krasnovik <Krasnovik@outlook.com>
2023-11-22 12:50:55 +0300

    SECOND LAB

--- Sem4_myLab2_OOP/Sem4_myLab2_OOP.cpp
+++ Sem4_myLab2_OOP/Sem4_myLab2_OOP.cpp
@@ -65,14 +65,6 @@ int main() {
 			Point* currentFace = ptrFaces[i];												   // нынешнее лицо
 			currentFace->Show();															   // показать нынешнее лицо
 
-			// матрица переходов (ОНА ДЛЯ 3 И 4 ЛАБЫ), строки - это сапоги, столбцы - это геометрические фигуры
-			//int transitionMatrix[n + 1][n] = {
-			//	{1, 2, 3},
-			//	{3, 2, 1},
-			//	{1, 3, 2},
-			//	{1, 2, 3}
-			//};
-
 			//окрашиваем фигуры в цвета
 			ptrGeometry[0]->Show();                    // показать прямоугольник
 			ptrGeometry[1]->Show();                    // показать квадрат
@@ -87,27 +79,8 @@ int main() {
 					Point* Object = ptrGeometry[j];		// геометрический объект
 					if (ptrGeometry[j]->IsCollision(ptrFaces[i], ptrGeometry[j]) && ptrGeometry[j]->IsVisible()) {	// если лицо столкнулось с фигурой, то
 
-						std::cout << "Произошло столкновение!" << std::endl;		// ВОТ ЭТИ ДВЕ СТРОЧКИ ДЛЯ 2 ЛАБЫ
-						return 0;													// ОСТАЛЬНОЕ ВСЕ ТУТ ДЛЯ 3 ЛАБЫ
-						
-						//ptrGeometry[j]->Hide();			// прячем геометрическую фигуру
-						//ptrGeometry[j]->MoveTo(ptrGeometry[j]->GetX() + rand() % 100 - 2, ptrGeometry[j]->GetY() + rand() % 100 - 2, 0, 0);
-						//// если фигура вышла за пределы поля, то возвращаем ее обратно
-						//if ((ptrGeometry[j]->GetX() + 30 > X_MAX) || (ptrGeometry[j]->GetX() - 30 < X_MIN) || (ptrGeometry[j]->GetY() - 40 > Y_MAX) || (ptrGeometry[j]->GetY() + 40 < Y_MIN)) {
-						//	ptrGeometry[j]->MoveTo(100 + rand() % 1000, 100, 0, 0);
-						//}
-
-						//ptrGeometry[j]->Show();	// показываем фигуру
-
-						//currentFace->Hide();			// прячем прошлые сапоги
-						//Point* NewBoots = ptrFaces[transitionMatrix[i][j]]; // новые сапоги
-						//NewBoots->SetX(currentFace->GetX()); // присваиваем новым сапогам координаты старых
-						//NewBoots->SetY(currentFace->GetY());
-						//NewBoots->SetXright(currentFace->GetXr());
-						//NewBoots->SetYright(currentFace->GetYr());
-						//i = transitionMatrix[i][j];		// i - индекс следующих сапог
-						//currentFace = NewBoots;		// текущие сапоги = новые сапоги
-						//currentFace->Show();			// показываем текующие сапоги
+						std::cout << "Произошло столкновение!" << std::endl;		
+						return 0;													
 					}
 				} 
 			} 
